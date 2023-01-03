4d02c92bdf478004a6a4074ddff02450b8cef84a
Dip Ghosh <dip.ghosh@shohoz.com>
2023-01-03 21:20:41 +0600

    read me added

---
+++ README.md
@@ -0,0 +1,38 @@
+<p align="center">
+<a href="https://laravel.com" target="_blank">
+<img src="QR.png" width="400" alt="QR Scanner Logo"></a></p>
+
+<p align="center">
+<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/v/laravel/framework" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
+</p>
+
+## About QR Scanner
+
+This is a pure javascript project. Tailwind css, Pure Javascripts and QR code package is used here
+
+
+## Sponsors
+
+We would like to extend our thanks to the following sponsors for funding development. 
+If you are interested in becoming a sponsor, please visit the  [https://www.linkedin.com/in/dip-ghosh/).
+
+
+
+## Contributing
+
+Thank you for considering contributing to QR CODE Scanner! 
+
+## Code of Conduct
+
+
+
+## Security Vulnerabilities
+
+If you discover a security vulnerability within Laravel, please send an e-mail to Dip Ghosh via(mailto:dipghosh638@gmail.com). All security vulnerabilities will be promptly addressed.
+
+## License
+
+This is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
--- js/app.js
+++ js/app.js
@@ -3,28 +3,29 @@ const form        = document.getElementById('generate-form');
 const qr          = document.getElementById('qrcode');
 const spinner     = document.getElementById('spinner')
 
+
 const onGenerateSubmit = (event) => {
     event.preventDefault();
 
     let url = document.getElementById('url').value;
     let size = document.getElementById('size').value;
+    let btn =  document.getElementById('save-link');
 
     if (url === '') {
         alert(" Url is required. ")
     }
     else {
+
+        if (btn)  clearUI();
         spinnerShow();
-        clearUI();
+
         setTimeout(() => {
             spinnerHide();
             generateQRCode(url, size);
-            createSaveBtn(url);
-            setTimeout( ()=> {
-                const saveUrl = qr.querySelector('img').src;
-                if (saveUrl) {
-                    createSaveBtn(saveUrl);
-                }
 
+            setTimeout( ()=> {
+                let saveUrl = qr.querySelector('img').src;
+                createSaveBtn(saveUrl);
             },50)
         }, 1000);
     }
@@ -38,13 +39,16 @@ const generateQRCode = (url, size) => {
     });
 }
 
-const createSaveBtn = (saveUrl) => {
-    const link     = document.createElement('a');
+
+let createSaveBtn = (saveUrl) => {
+    let link     = document.createElement('a');
     link.id        = 'save-link';
-    link.classList = 'bf-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    link.classList = 'bf-red-500 bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5 btn btn-info';
     link.href      = saveUrl;
+    link.type      ='btn btn-sm'
     link.download  = 'qrcode';
     link.innerHTML = 'Save Image';
+
     document.getElementById('generated').appendChild(link);
 }
 
@@ -56,9 +60,9 @@ let spinnerHide = () => {
     spinner.style.display = 'none';
 }
 
-const clearUI     = () => {
+const clearUI = () => {
     qr.innerHTML= '';
-    // document.getElementById('generated').remove();
+    document.getElementById('save-link').remove();
 }
 
 form.addEventListener('submit', onGenerateSubmit);
